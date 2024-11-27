059211135ae63f79651e6dd19a37e7c1f1d0bb9e
Christian Jimenez <cajt@outlook.com>
2024-11-27 12:01:01 -0800

    Excercise JQERY/Ajax

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 $(function() {
   // your code here
+  // acordion
+  $(".accordion > .accordion-header").on("click", function(){
+    $(".accordion-content").not($(this).next()).slideUp()
+    if(($(this).next()).is(":hidden")==true){
+      $(this).next().slideDown()
+    }
+    else{
+      $(this).next().slideUp()
+    }
+  })
+  // fetch data
+  $(".todos").find("button").on("click",()=>{
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      type: 'GET',
+      success: function
+      (response){
+        arr = response.todos
+        arr.forEach(element => {
+          $(".todos").find("ul").append(`<li>${element.todo}</li>`)
+        });
+      },
+      error: function(err){
+        console.error(err)
+      }
+    })
+  })
 })
